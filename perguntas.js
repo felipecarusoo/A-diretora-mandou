b6f174ef3dec0b78c4dd746e0a21f997f0b84d8a
Wonder <101845224+felipecarusoo@users.noreply.github.com>
2024-12-03 19:20:53 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,127 @@
+criaCartao(
+    '01', 
+    'A diretora mandou...', 
+    'Pular de um pé só.',
+    '1.png',
+    'fake' /*Se falso envie fake*/ 
+)
+
+criaCartao(
+    '02', 
+    'A diretora mandou...', 
+    'Tocar em algo vermelho.',
+    '2.png',
+    ''
+)
+
+criaCartao(
+    '03', 
+    'A diretora mandou...', 
+    'Fingir que está lutando box.',
+    '3.png',
+    ''
+)
+
+criaCartao(
+    '04', 
+    'A diretora mandou...', 
+    'Abraçar alguém.',
+    '4.png',
+    ''
+)
+
+criaCartao(
+    '05', 
+    'A diretora mandou...', 
+    'Imitar um carro velho.',
+    '5.png',
+    ''
+)
+
+criaCartao(
+    '06', 
+    'A diretora mandou...', 
+    'Segurar a respiração por 10seg.',
+    '6.png',
+    'fake'
+)
+
+criaCartao(
+    '07', 
+    'A diretora mandou...', 
+    'Falar três planetas.',
+    '7.png',
+    ''
+)
+
+criaCartao(
+    '08', 
+    'A diretora mandou...', 
+    'Gritar: Vamos!',
+    'vanessa.png',
+    'fake'
+)
+
+criaCartao(
+    '09', 
+    'A diretora mandou...', 
+    'Falar "paralelepípedo" ao contrário.',
+    '9.png',
+    ''
+)
+
+criaCartao(
+    '10', 
+    'A diretora mandou...', 
+    'Imitar o sinal da escola.',
+    '10.png',
+    'fake'
+)
+
+criaCartao(
+    '11',   
+    'A diretora mandou...', 
+    'Gritar: Eu nunca mais vou vir sem uniforme.',
+    'michele.png',
+    ''
+)
+
+criaCartao(
+    '12', 
+    'A diretora mandou...', 
+    'Se fingir de morto.',
+    '12.png',
+    ''
+)
+
+criaCartao(
+    '13', 
+    'A diretora mandou...', 
+    'Imitar um robô.',
+    '13.png',
+    ''
+)
+
+criaCartao(
+    '14', 
+    'A diretora mandou...', 
+    'Fazer uma pose.',
+    '14.png',
+    'fake'
+)
+
+criaCartao(
+    '15', 
+    'A diretora mandou...', 
+    'Citar os 5 continentes.',
+    '15.png',
+    ''
+)
+
+criaCartao(
+    '16', 
+    'A diretora mandou...', 
+    'Cantar um refrão de "Evidências": E nessa loucura de dizer que não te quero...',
+    '16.png',
+    ''
+)
